0b96b1ef600f0786f4f309c66d52a55edc0a4f96
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2020-10-01 12:44:27 +0200

    Testing the first map edits

--- maps/CinnabarIsland.asm
+++ maps/CinnabarIsland.asm
@@ -23,6 +23,9 @@ CinnabarIslandBlue:
 	clearevent EVENT_VIRIDIAN_GYM_BLUE
 	end
 
+CinnabarBoulder:
+	jumpstd StrengthBoulderScript
+
 CinnabarIslandGymSign:
 	jumptext CinnabarIslandGymSignText
 
@@ -141,3 +144,8 @@ CinnabarIsland_MapEvents:
 
 	def_object_events
 	object_event  9,  6, SPRITE_BLUE, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarIslandBlue, EVENT_BLUE_IN_CINNABAR
+	object_event  5, 11, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarBoulder, -1
+	object_event  8, 11, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarBoulder, -1
+	object_event  6,  8, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarBoulder, -1
+	object_event  6, 10, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarBoulder, -1
+	object_event 13, 12, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarBoulder, -1
