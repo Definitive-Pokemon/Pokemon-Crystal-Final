b160323018d2d9eeae00924d410f58a20ad83821
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2020-11-25 12:37:16 +0100

    Connections

--- maps/ScarletMart.asm
+++ maps/ScarletMart.asm
@@ -16,8 +16,8 @@ ScarletMart_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  2,  7, SCARLET_CITY, 2
-	warp_event  3,  7, SCARLET_CITY, 2
+	warp_event  2,  7, SCARLET_CITY, 1
+	warp_event  3,  7, SCARLET_CITY, 1
 
 	def_coord_events
 
