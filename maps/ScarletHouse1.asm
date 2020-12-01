b716392fe41098298632e0e682dba38e3d009a5d
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2020-12-01 16:37:52 +0100

    House NPCs added

--- maps/ScarletHouse1.asm
+++ maps/ScarletHouse1.asm
@@ -1,9 +1,39 @@
 	object_const_def
+	const SCARLETHOUSE1_BLACK_BELT
+	const SCARLETHOUSE1_BEAUTY
 
 ScarletHouse1_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+ScarletHouse1BlackBeltScript:
+	jumptextfaceplayer ScarletHouse1BlackBeltText
+
+ScarletHouse1BeautyScript:
+	jumptextfaceplayer ScarletHouse1BeautyText
+
+ScarletHouse1BlackBeltText:
+	text: "Darn it!"
+
+	para "I keep losing at the"
+	line "very end of the"
+	cont "BATTLE TOWER."
+
+	para "I hate having to start"
+	line "all over again!"
+
+	para "It's just not fair!"
+	done
+
+ScarletHouse1BeautyText:
+	text "Instead of complaining"
+	line "about his losses,"
+
+	para "how about he took"
+	line "some more time"
+	cont "out for me?"
+	done
 
 ScarletHouse1_MapEvents:
 	db 0, 0 ; filler
@@ -17,3 +47,5 @@ ScarletHouse1_MapEvents:
 	def_bg_events
 
 	def_object_events
+	object_event  1,  4, SPRITE_BLACK_BELT, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, ScarletHouse1BlackBeltScript, -1
+	object_event  5,  3, SPRITE_BEAUTY, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, ScarletHouse1BeautyScript, -1
--- maps/ScarletOldHouse.asm
+++ maps/ScarletOldHouse.asm
@@ -1,9 +1,37 @@
 	object_const_def
+	const SCARLETOLDHOUSE_GRANNY
+
 
 ScarletOldHouse_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+ScarletOldHouseGrannyScript:
+	jumptextfaceplayer ScarletOldHouseGrannyText
+
+ScarletOldHouseGrannyText:
+	text "Before this city"
+	line "was built, this"
+
+	para "house was the"
+	line "only one standing"
+	cont "here."
+
+	para "It's been inhabited"
+	line "by a line of"
+
+	para "protectors of"
+	line "the WHIRL ISLANDS."
+
+	para "I am the latest"
+	line "of this line."
+
+	para "And I still protect"
+	line "these islands."
+
+	para "So don't provoke me."
+	done
 
 ScarletOldHouse_MapEvents:
 	db 0, 0 ; filler
@@ -17,3 +45,4 @@ ScarletOldHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
+	object_event  2,  3, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ScarletOldHouseGrannyScript, -1
