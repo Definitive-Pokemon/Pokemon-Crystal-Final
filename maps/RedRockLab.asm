00a0ac0025d82165d31b65260cead79fa1694976
Thirteen1355 <dertienvijfenvijftig@gmail.com>
2020-12-02 16:22:21 +0100

    Updated

--- maps/RedRockLab.asm
+++ maps/RedRockLab.asm
@@ -60,37 +60,43 @@ RedRockLabScientist1Text:
 
 RedRockLabScientist2Text:
 	text "We managed to"
-	line "breed with the #-"
+	line "breed with the"
 
-	para "MON we received"
-	line "from TRAINER RED."
+	para "#MON we"
+	line "received from"
+	cont "TRAINER RED."
 
 	para "FOSSILS are in-"
 	line "credibly rare."
 
-	para "But with our breeding"
-	line "program, we barely"
-	cont "even need them!"
+	para "But with our bree-"
+	line "ing program,"
+
+	para "we barely even"
+	line "need them!"
 	done
 
 RedRockLabScientist3Text:
-	text "We sure manage"
-	line "to make a lot of"
+	text "We sure manage to"
+	line "make a lot of"
 
 	para "money selling"
 	line "FOSSIL #MON"
 	cont "to TRAINERS."
 
 	para "This, in turn,"
-	line "allows us to con"
+	line "allows us to con-"
 
 	para "tinue to raise"
 	line "these #MON."
+
+	para "It's real charity"
+	line "work."
 	done
 
 RedRockLabScientist4Text:
-	text "I'm very sorry,"
-	line "but I can't allow"
+	text "I'm very sorry, but"
+	line "I can't allow"
 
 	para "access to the"
 	line "BREEDING GROUNDS."
@@ -113,7 +119,7 @@ RedRockLab_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  4,  5, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RedRockLabScientist1Script, -1
-	object_event  4,  2, SPRITE_SCIENTIST, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RedRockLabScientist2Script, -1
+	object_event  4,  2, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RedRockLabScientist1Script, -1
+	object_event  4,  5, SPRITE_SCIENTIST, SPRITEMOVEDATA_WANDER, 2, 2, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RedRockLabScientist2Script, -1
 	object_event  9,  4, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RedRockLabScientist3Script, -1
 	object_event 10,  6, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RedRockLabScientist4Script, -1
