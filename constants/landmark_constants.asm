06c01766f32ef19579b5edf15354c66307d7ce15
ElwinBran <elwinbran@gmail.com>
2021-01-09 20:59:08 +0100

    Solved Scarlet/Olivine City Fly glitch, allong with tiny style fix

--- constants/landmark_constants.asm
+++ constants/landmark_constants.asm
@@ -33,10 +33,10 @@
 	const LANDMARK_LIGHTHOUSE        ; 1c
 	const LANDMARK_BATTLE_TOWER      ; 1d
 	const LANDMARK_ROUTE_40          ; 1e
-	const LANDMARK_RED_ROCK_ISLE          ; 1f
-	const LANDMARK_SCARLET_CITY          ; 20
-	const LANDMARK_RED_ROCK_OUTLOOK         ; 21
-	const LANDMARK_SEARING_CRATER         ; 22
+	const LANDMARK_RED_ROCK_ISLE     ; 1f
+	const LANDMARK_SCARLET_CITY      ; 20
+	const LANDMARK_RED_ROCK_OUTLOOK  ; 21
+	const LANDMARK_SEARING_CRATER    ; 22
 	const LANDMARK_ROUTE_41          ; 23
 	const LANDMARK_WHIRL_ISLANDS     ; 24
 	const LANDMARK_CIANWOOD_CITY     ; 25
